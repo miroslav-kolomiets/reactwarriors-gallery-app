6f332370ede19d07962388a75e4950511288c9ef
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2019-11-19 13:21:11 +0200

    Add filter

--- src/components/RangeInput.js
+++ src/components/RangeInput.js
@@ -4,12 +4,12 @@ import { FormGroup, Label, CustomInput } from "reactstrap";
 const RangeInput = props => {
   return (
     <FormGroup>
-      <Label for="exampleCustomRange">Custom Range</Label>
+      <Label for="exampleCustomRange">{props.value}</Label>
       <CustomInput
         type="range"
         id="exampleCustomRange"
         name="customRange"
-        max={20}
+        max={props.max}
         min={0}
         value={props.value}
         onChange={props.handleChange}
