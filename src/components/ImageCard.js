8910699c5013d723fdd653abab735bdc938e7c36
MiroslavKolomiets <kolomiets.miroslav@pdffiller.team>
2019-11-19 14:08:58 +0200

    Fix filter bugs

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -15,18 +15,11 @@ const ImageCard = props => {
       <Card>
         <CardBody>
           {card.thumbnail.length > 10 && (
-            <CardImg
-              top
-              width="100%"
-              src={card.thumbnail}
-              alt="Card image"
-            />
+            <CardImg top width="100%" src={card.thumbnail} alt="Card image" />
           )}
 
           <CardTitle>{card.title}</CardTitle>
-          <CardSubtitle>
-            Number of comments: {card.num_comments}
-          </CardSubtitle>
+          <CardSubtitle>Number of comments: {card.num_comments}</CardSubtitle>
           <NavLink href={`https://www.reddit.com/${card.permalink}`}>
             Link
           </NavLink>
